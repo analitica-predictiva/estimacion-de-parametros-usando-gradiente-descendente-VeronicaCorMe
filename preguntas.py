11eac4f34836a09ccfd79886c62e1d5d8f6fdff9
VeronicaCorMe <vcordobam@unal.edu.co>
2022-11-02 11:03:41 -0500

    Update preguntas.py
    Pregunta 2

--- preguntas.py
+++ preguntas.py
@@ -7,6 +7,7 @@ polinomial de grado `n`.
 
 """
 
+
 def pregunta_01():
     """
     Complete el código presentado a continuación.
@@ -30,8 +31,6 @@ def pregunta_01():
     return x_poly, data.y
 
 
-
-
 def pregunta_02():
 
     # Importe numpy
@@ -40,21 +39,21 @@ def pregunta_02():
     x_poly, y = pregunta_01()
 
     # Fije la tasa de aprendizaje en 0.0001 y el número de iteraciones en 1000
-    learning_rate = ___
-    n_iterations = ___
+    learning_rate = 0.0001
+    n_iterations = 1000
 
     # Defina el parámetro inicial `params` como un arreglo de tamaño 3 con ceros
-    params = np.___(___.shape[1])
-    for _ in range(n_iterations):
+    params = np.array([0, 0, 0])
+    for n in range(n_iterations):
 
-        # Compute el pronóstico con los parámetros actuales
-        y_pred = np.___(___, ___)
+        # Compute el pronóstico con los parámetros actuales (x * parametros)
+        y_pred = np.dot(x_poly, params)
 
-        # Calcule el error
-        error = ___ - ___
+        # Calcule el error (predicción- real)
+        error = y_pred - y
 
-        # Calcule el gradiente
-        gradient = ____
+        # Calcule el gradiente (trasnpuesta x * el error)
+        gradient = np.dot(x_poly.T, error)
 
         # Actualice los parámetros
         params = params - learning_rate * gradient
